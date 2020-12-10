e62895595080593c4aed3431901ec4ebce32f361
jaredc23 <38102180+jaredc23@users.noreply.github.com>
2020-12-10 00:27:59 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.getElementById("navBar").style.padding = "30px 10px";
       document.getElementById("logo").style.fontSize = "25px";
-      document.getElementById("navBar").style.boxShadow = "0 20px 20px -2px rgba(0,0,0,.2)";
+      document.getElementById("navBar").style.boxShadow = "0 20px 20px -2px rgba(0,0,0,.4)";
       document.getElementById("navBar-right").style.fontSize = 30;
     } else {
-      document.getElementById("navBar").style.padding = "80px 10px";
+      document.getElementById("navBar").style.padding = "70px 10px";
       document.getElementById("logo").style.fontSize = "60px";
       document.getElementById("navBar").style.boxShadow = "0 3px 3px -2px rgba(0,0,0,.2)";
       document.getElementById("navBar-right").style.fontSize = 50;
