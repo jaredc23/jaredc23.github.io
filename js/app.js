e7bbdee6128a9b4af70384f80b71af157199ac6b
jaredc23 <38102180+jaredc23@users.noreply.github.com>
2020-12-09 23:30:23 -0500

    Changes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function()
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("navBar").style.padding = "30px 10px";
+      document.getElementById("logo").style.fontSize = "25px";
+      document.getElementById("navBar").style.boxShadow = "0 10px 10px -2px rgba(0,0,0,.2)";
+    } else {
+      document.getElementById("navBar").style.padding = "80px 10px";
+      document.getElementById("logo").style.fontSize = "60px";
+      document.getElementById("navBar").style.boxShadow = "0 3px 3px -2px rgba(0,0,0,.2)";
+    }
+};
+
+
+function navBarOnClick(id)
 {
-    document.body.classList.toggle('dark-theme')
-    document.body.classList.toggle('light-theme')
-
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
-
-    console.log('current class name: ' + className);
-});
+    document.getElementsByClassName('active')[0].classList.toggle('active');
+    document.getElementById(id).classList.toggle('active');
+}
